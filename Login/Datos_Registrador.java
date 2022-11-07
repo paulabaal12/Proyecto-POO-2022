eb280963586a57bfb629996aeac3f311fb2948f1
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2022-11-07 17:39:51 -0600

    .

---
+++ Login/Datos_Registrador.java
@@ -0,0 +1,20 @@
+public class Datos_Registrador {
+
+        private String Usuario;
+        private String Contraseña;
+        
+        public String getContraseña() {
+            return Contraseña;
+        }
+        public void setContraseña(String contraseña) {
+            Contraseña = contraseña;
+        }
+        public String getUsuario() {
+            return Usuario;
+        }
+        public void setUsuario(String usuario) {
+            Usuario = usuario;
+        }
+    
+    
+}
---
+++ Login/Main.java
@@ -0,0 +1,10 @@
+import javax.sound.sampled.SourceDataLine;
+
+public class Main {
+    public static void main(String[] args){
+    Registrador registrador = new Registrador();
+    registrador.registra();
+  //  registrador.Datos_Registrados();
+    
+    }
+}
