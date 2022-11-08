e81e0e0a2e3d1ba06a2958999a244e0da6fdc2bd
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2022-11-07 23:48:24 -0600

    vocales

---
+++ src/Functionalities/Language.java
@@ -0,0 +1,29 @@
+package Functionalities;
+/******************************************************************
+Proyecto POO
+Autores: 
+    Ana Paula Navas #22731
+    Sergio Palacios #22808
+    Paula Rebeca Barillas #22764
+
+Última modificación: 9/10/2022
+ 
+Driver program/clase sobre Idioma Español, la cuál va a heredar otras 
+******************************************************************/
+public class Language {
+
+	    String vocales;
+	    String consonantes;
+	    public String getVocales() {
+	        return vocales;
+	    }
+	    public void setVocales(String vocales) {
+	        this.vocales = vocales;
+	    }
+	    public String getConsonantes() {
+	        return consonantes;
+	    }
+	    public void setConsonantes(String consonantes) {
+	        this.consonantes = consonantes;
+	    }
+	}
