fd7993c9c0fa3e5192f4cc3456143de64b6bd4cb
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2022-11-07 23:22:58 -0600

    Creación y cambios en el controller, main

---
+++ Main.java
@@ -0,0 +1,11 @@
+//import DataStore.OpenFiles;
+
+public class Main {
+	public static void main(String[] args) {
+		Controller controller = new Controller();
+		controller.start();
+//		OpenFiles files = new OpenFiles();
+//		files.crear_archivo("Users");
+		
+	}
+}
