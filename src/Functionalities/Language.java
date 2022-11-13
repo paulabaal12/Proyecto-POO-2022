35f2de0d7d1541d32a8aff1ae631539adbb66cd1
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2022-11-13 13:11:59 -0600

    Update Language.java
    .

--- src/Functionalities/Language.java
+++ src/Functionalities/Language.java
@@ -10,20 +10,51 @@ Autores:
  
 Driver program/clase 
 ******************************************************************/
+
+import java.util.stream.*;
+import javax.swing.JOptionPane;
+import java.util.ArrayList;
+
 public class Language {
 
-	    String vocales;
-	    String consonantes;
-	    public String getVocales() {
-	        return vocales;
-	    }
-	    public void setVocales(String vocales) {
-	        this.vocales = vocales;
-	    }
-	    public String getConsonantes() {
-	        return consonantes;
-	    }
-	    public void setConsonantes(String consonantes) {
-	        this.consonantes = consonantes;
-	    }
-	}
+	String[] vocales = {"a","e","i","o","u"};
+	String[] consonantes = {"b","c","d","f","g","h","j","k","l","m","n","ñ","p","q","r","s","t","v","w","x","y","z"};
+	String[] ABC = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p","q","r","s","t","u","v","w","x","y","z"};
+	public void lengua(){
+	do {
+		option = Integer.parseInt(JOptionPane.showInputDialog(
+				"Menu Lenguaje\n"
+						+ "1. Vocales\n"
+						+ "2. Consonantes \n"
+						+ "3. Abecedario Completo \n"
+						+ "4. Ejemplos de la palabra seleccionada \n"
+						+ "5. Escritura de palabras, ,manera correcta \n"
+						+ "6. Salir \n"));
+
+		switch (option) {
+			case 1:
+			//Vocales
+				break;
+			case 2:
+			//Consonantes
+				break;
+
+			case 3:
+			// Abecedario completo
+				break;
+
+			case 4:
+			//Ejemplos de la palabra seleccionada
+				break;
+
+			case 5:
+			//Escritura de palabras, ,manera correcta
+				break;
+			case 6:
+				break;
+		}
+
+	} while (option != 5);
+
+}
+}
