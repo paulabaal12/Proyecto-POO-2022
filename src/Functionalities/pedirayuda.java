6adb2837ce336c41b7002dc3270f71dab9bf5bb7
Sergio2303 <105880533+Sergio2303@users.noreply.github.com>
2022-11-14 12:23:19 -0600

    Opción para pedir Ayuda
    ;)

--- src/Functionalities/pedirayuda.java
+++ src/Functionalities/pedirayuda.java
@@ -10,13 +10,18 @@ Autores:
  
 Driver program/clase 
 ******************************************************************/
-
+import java.util.Scanner;
 import java.util.stream.*;
 import javax.swing.JOptionPane;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 import java.util.ArrayList;
+
 
 public class pedirayuda {
     int option = 0; 
+    Scanner sc = new Scanner();
+    Registro_Ayuda regist = new Registro_Ayuda();
     public void ayuda(){
         do {
             option = Integer.parseInt(JOptionPane.showInputDialog(
@@ -29,19 +34,50 @@ public class pedirayuda {
     
             switch (option) {
                 case 1:
-                //Registro para dar ayuda
+                    String register = sc.nextLine();
+                    System.out.println("---Bienvenido Por favor ingresa tus datos---");
+                    
+                    System.out.println("Nombre: ");
+                    String nombre = sc.nextLine();
+                    register.setNombre(nombre);
+
+                    System.out.println("Apellido: ");
+                    String apellido = sc.nextLine();
+                    register.setApellido(apellido);
+
+                    System.out.println("Numero de Teléfono: ");
+                    String telefono = sc.nextLine();
+                    register.setNumero_tele(telefono);
+
+                    System.out.println("DPI: ");
+                    String dpi = sc.nextLine();
+                    register.setDPI(dpi);
+
                      break;
 
                 case 2:
-                //Buscar ayuda
+                System.out.println("Bienvenid@, Por favor cuéntanos a continuación en que te podemos ayudar: \n");
+                String data = sc.nextLine();
+                register.setData(data);
+
+                System.out.println("Coméntanos que piensas sobre nuestro servicio: \n");
+                String opinion = sc.nextLine();
+                register.setOpinion(opinion);
                      break;
                 
                 case 3:
-                //Lista de centros de educación públicos
+                System.out.println("Nuestros Centros de educación públicos: ");
+                System.out.println("1. Centro Principal \nCalzada Roosevelt, Cdad. de Guatemala 01011 \nNumero Telefónico: 2285-4302");
+                System.out.println("2. Centro Buena Vista \nGuatemala, 13 Calle 8, Guatemala \nNumero Telefónico: 2375-5728");
+                System.out.println("3. Centro Unidos Por Un mejor Día \nJ2j6+P62, Jalapa\nNumero Telefónico: 7845-4173");
                      break;
                 
                 case 4:
-                //Lista de lugares para contribuir
+                System.out.println("---Si deseas contribuir a nuestra organización, te dejamos los datos para que puedas hacerlo, de antemano te agradecemos tu colarboración.---");
+                System.out.println("Dirección para contribuciones: \n1. Centro Principal \nCalzada Roosevelt, Cdad. de Guatemala 01011 \nNumero Telefónico: 2285-4302 \n2. Centro Unidos Por Un mejor Día \nJ2j6+P62, Jalapa\nNumero Telefónico: 7845-4173");
+                System.out.println("¿Tienes algun comentario o opinon sobre las contribuciones? Dejanos saber a continuación: ");
+                String coments = sc.nextLine();
+                register.setComentario(coments);
                      break;
 
                 case 5:
@@ -53,6 +89,3 @@ public class pedirayuda {
     }
     }
     
-
-
-
